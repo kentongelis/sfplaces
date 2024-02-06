2e1006e916e93aacfd58f6f3dbaeea4871cdd641
kentongelis <kenton.gelis@students.dominican.edu>
2024-02-06 11:14:51 -0800

    header component built

---
+++ src/Title.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Title.css';
+
+function Title() {
+    return (
+        <div className="Title">
+            <h1>SFPOPOS</h1> 
+            <div className="Title-Subtitle"></div>
+                <h2>San Fransiscos Privately Owned Public Spaces</h2>
+        </div>
+    )
+}
+
+export default Title
