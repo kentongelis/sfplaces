a5ee42cb7ec0d9dbf814de31d7ba0a731d760405
kentongelis <kenton.gelis@students.dominican.edu>
2024-02-13 01:13:15 -0800

    search bar

--- src/components/POPOSList/POPOSList.js
+++ src/components/POPOSList/POPOSList.js
@@ -1,14 +1,18 @@
 import POPOSSpace from '../POPOSSpace/POPOSSpace.js'
 import './POPOSList.css';
-import data from '../../sfpopos-data.json'
+import { useState } from 'react'
+import data from '../../sfpopos-data.js'
 
 function POPOSList() {
-  const spaces = data.map(( { title, address, images, hours }, i ) => {
+  const [query, setQuery ] = useState('')
+  const spaces = data
+  .filter(obj => obj.title.toLowerCase().includes(query.toLowerCase()) || obj.address.toLowerCase().includes(query.toLowerCase()))
+  .map(( { id, title, address, images, hours } ) => {
 
     return (
       <POPOSSpace
-        id={i}
-        key={title}
+        id={id}
+        key={`%{title}-${id}`}
         name={title}
         address={address}
         image={images[0]}
@@ -19,6 +23,14 @@ function POPOSList() {
 
   return (
     <div className="POPOSList">
+      <form>
+        <input
+            value={query}
+            placeholder='search'
+            onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
       { spaces }
     </div>
   )
