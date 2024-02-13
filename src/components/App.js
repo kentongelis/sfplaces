619021b1b2202200eaaace630c61ccd472c68c78
kentongelis <kenton.gelis@students.dominican.edu>
2024-02-12 23:59:32 -0800

    fixed file layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom'
 import './App.css';
-import Title from './Title.js';
+import Title from './Title/Title.js';
+import Footer from './Footer/Footer.js';
 
 function App() {
   return (
     <div className="App">
       <Title />
       <Outlet />
+      <Footer />
     </div>
   );
 }
--- src/components/POPOSList/POPOSList.js
+++ src/components/POPOSList/POPOSList.js
@@ -1,6 +1,6 @@
-import POPOSSpace from './POPOSSpace.js'
+import POPOSSpace from '../POPOSSpace/POPOSSpace.js'
 import './POPOSList.css';
-import data from './sfpopos-data.json'
+import data from '../../sfpopos-data.json'
 
 function POPOSList() {
   const spaces = data.map(( { title, address, images, hours }, i ) => {
