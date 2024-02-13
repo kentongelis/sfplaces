50822394f1310e94b212384434133a2cdabeae56
kentongelis <kenton.gelis@students.dominican.edu>
2024-02-13 00:16:37 -0800

    styled list page

--- src/components/POPOSSpace/POPOSSpace.js
+++ src/components/POPOSSpace/POPOSSpace.js
@@ -4,17 +4,21 @@ import { Link } from 'react-router-dom'
 function POPOSSpace(props) {
     const { name, image, address, hours, id } = props
     return (
-        <div>
+        <div className="POPOSSpace">
             <Link to={`/details/${id}`}>
             <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
             </Link>
             <h1>
-                <Link to={`/details/${id}`}>
+                <Link 
+                    className="POPOSSpace-title"
+                    to={`/details/${id}`}>
                     {name}
                 </Link>
             </h1>
-            <div>{address}</div>
-            <p><small>{hours}</small></p>
+            <div className="POPOSSpace-info">
+                <div>{address}</div>
+                <div>{hours}</div>
+            </div>
         </div>
     )
 }
